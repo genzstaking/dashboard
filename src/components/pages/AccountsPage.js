859a570c84679339fc8dfb247ad9e6bab041e629
razi jafari <razi.jafari68@gmail.com>
2022-12-10 17:19:21 +0330

    for test

--- src/components/pages/AccountsPage.js
+++ src/components/pages/AccountsPage.js
@@ -4,5 +4,6 @@ import { xml } from "@odoo/owl";
 export class AccountsPage extends Component{
     static template = xml `
         <button class="primary">info</button>
+        <p>h</p>
     `;
 }
