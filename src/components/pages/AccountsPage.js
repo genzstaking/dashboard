097e00c14288eedac173e95ba255017f6cf2152c
razi jafari <razi.jafari68@gmail.com>
2022-12-10 19:01:31 +0330

    add some button for checking install to metamask and allow to conect it's wallet"

--- src/components/pages/AccountsPage.js
+++ src/components/pages/AccountsPage.js
@@ -3,10 +3,22 @@ import { xml } from "@odoo/owl";
 
 export class AccountsPage extends Component{
     static template = xml `
-        <button class="btn btn-primary" t-on-click="toggleTask">info</button>
-        
+        <button class="btn btn-primary" t-on-click="checkConectToMetamask">info</button>
+        <button class="btn btn-info" t-on-click="allowToConect">conect</button>
     `;
-    toggleTask(){
-        alert('hhklkj')
+    checkConectToMetamask(){
+        // a thing realated ethereum is installed
+        if (window.ethereum){
+            console.log('is exist')
+        };
+        //check for metamask is install
+        if (window.ethereum.isMetaMask){
+            console.log('metamask') 
+        } 
+    };
+    allowToConect(){
+        // allow to conect wallet to metamask
+        ethereum.request({ method: 'eth_requestAccounts' });
+
     }
 }
